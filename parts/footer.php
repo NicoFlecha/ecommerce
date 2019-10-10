e914d20f98165504f78c1ef8f015922794809733
Nico Flecha <nicoflechacontacto@gmail.com>
2019-10-10 11:26:57 -0300

    Empiezo nuevo proyecto

---
+++ parts/footer.php
@@ -0,0 +1,27 @@
+<div class="logo-container">
+  <img src="images/brand-logo.png" alt="logo">
+</div>
+<div class="texto-contacto">
+<div class="box1">
+  <h3>Atención al Cliente:</h3>
+  <a href="tel:0800-999-9999">0800-999-9999</a>
+</div>
+<div class="box2">
+  <h3>Otro numero de Contacto:</h3>
+  <a href="tel:0800-999-9999">0800-999-9999</a>
+</div>
+<div class="terminos">
+  <a href="terminos"><h3>Terminos y condiciones</h3></a>
+</div>
+<div class="politica">
+  <a href="politica"><h3>Politica y privacidad</h3></a>
+</div>
+</div>
+<hr>
+<div class="suscripcion">
+  <h3>Recibí ofertas y promociones por email</h3>
+  <form>
+    <input type="email" placeholder="Ingresá tu email" required>
+    <button type="submit">SUSCRIBIRME</button>
+  </form>
+</div>
---
+++ parts/header.php
@@ -0,0 +1,16 @@
+    <header id="header">
+      <nav class="menuHeader">
+        <div class="logo">
+          <a href="#">
+            <img src="img/logo.png" alt="">
+          </a>
+        </div>
+        <div class="enlaces">
+          <a href="#"><i class="fas fa-fire"></i> Productos</a>
+          <a href="#"><i class="far fa-question-circle"></i> F.A.Q</a>
+          <a href="#"><i class="fas fa-user"></i> Ingresar</a>
+          <a href="#"><i class="fas fa-user-plus"></i> Registrarse</a>
+          <a class="carrito" href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </nav>
+    </header>
