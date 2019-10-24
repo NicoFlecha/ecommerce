58a69fcbffa6f0b005db14ebcbb8c178c5f7936e
NicoFlecha <nicoflechacontacto@gmail.com>
2019-10-24 20:33:48 -0300

    Acomodo Footer, inicio Account.php

---
+++ account.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil de NOMBRE</title>
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/account.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <?php require_once('parts/header.php') ?>
+    <div class="relleno">
+    </div>
+    <div class="contenedorCuenta">
+      <div class="datos">
+        <img src="" alt="">
+        <h2>Nombre Apellido</h2>
+      </div>
+      <div class="compras">
+
+      </div>
+    </div>
+    <?php require_once('parts/footer.php') ?>
+  </body>
+</html>
